54881ad468c40a8c3767e9d56a3dc48ac2ebc78c
m-atefgeeklab <mohamedatefgeeklab@gmail.com>
2024-09-08 12:30:10 +0300

    changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,16 +15,18 @@ const {
 } = require("../utils/validators/authValidator.js");
 const { verifyEmailWebhook } = require("../services/authService.js");
 const createToken = require("../utils/createToken");
+const validatorMiddleware = require('../../middlewares/validationMiddleware');
 
 const router = express.Router();
 
 router.post("/signup", signupValidator, signUpController);
 router.post("/login", loginValidator, signInController);
-router.post("/with-google", googleLogin);
 router.get("/verify-email/:token", verifyEmailController);
 router.post("/forgot-password", forgotPassword);
 router.post("/verify-reset-code", verifyPassResetCode);
 router.put("/reset-password", resetPassword);
+
+router.post("/with-google", validatorMiddleware, googleLogin);
 
 router.post("/webhook/verify-email", verifyEmailWebhook);
 
