fd3b433eb7b1aee08294586b163041349d430b07
m-atefgeeklab <mohamedatefgeeklab@gmail.com>
2024-09-08 12:00:56 +0300

    changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -20,7 +20,7 @@ const router = express.Router();
 
 router.post("/signup", signupValidator, signUpController);
 router.post("/login", loginValidator, signInController);
-router.get("/with-google", googleLogin);
+router.post("/with-google", googleLogin);
 router.get("/verify-email/:token", verifyEmailController);
 router.post("/forgot-password", forgotPassword);
 router.post("/verify-reset-code", verifyPassResetCode);
